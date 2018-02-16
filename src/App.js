fb2345bdef3918ea7f75ee88aae9cf9130625bd1
Bruno Sinou <bruno.sinou@gmail.com>
2018-02-16 08:45:29 +0100

    Play with components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <h1>I react app</h1>
+       <p> this is working</p>
+       <Person name='Bruno' age='12' />
+       <Person name='Jackie' age='78' />
       </div>
     );
   }
